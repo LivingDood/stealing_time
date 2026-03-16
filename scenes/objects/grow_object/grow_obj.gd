02c96a7e49feff3e3930c1ae87f6d34ab392cd0c
LivingDood <264967182+LivingDood@users.noreply.github.com>
2026-03-16 08:48:10 +0100

    Update grow object to toggle highlight shader using instance parameter

--- scenes/objects/grow_object/grow_obj.gd
+++ scenes/objects/grow_object/grow_obj.gd
@@ -1,21 +1,25 @@
-class_name GrowObj extends AnimatableBody2D
+class_name GrowObj
+extends AnimatableBody2D
+
 @export var hitbox:CollisionPolygon2D
 @export var sprite:Sprite2D
 @export var animation_player:AnimationPlayer
-@export var highlight_shader:ShaderMaterial
-var grown:bool = false
 
+var grown:bool = false
 var selected:bool = false
+
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_enter)
 	mouse_exited.connect(_on_mouse_exit)
 
+
 func steal(seconds:float):
 	grown = true
 	animation_player.play("grow")
 	PlayerStats.add_time(seconds)
-	
+
+
 func give(seconds:float):
 	grown = false
 	animation_player.play_backwards("grow")
@@ -28,14 +32,13 @@ func _input(event: InputEvent) -> void:
 			steal(20)
 		elif grown && event.button_index == MOUSE_BUTTON_RIGHT:
 			give(20)
-			
-			
-			
+
 
 func _on_mouse_enter() -> void:
 	selected = true
-	sprite.material = highlight_shader;
-	
+	sprite.set_instance_shader_parameter("is_enabled", selected)
+
+
 func _on_mouse_exit() -> void:
 	selected = false
-	sprite.material = null
+	sprite.set_instance_shader_parameter("is_enabled", selected)
