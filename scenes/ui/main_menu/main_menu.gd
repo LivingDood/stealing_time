e3832b85de80585f0976a82d86b64eee5e21e3d7
LivingDood <264967182+LivingDood@users.noreply.github.com>
2026-03-22 07:57:34 +0100

    Update main_menu
    Add animations for aging character
    Add highlighting tree on mouse hovering
    Add stealing interaction

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -6,12 +6,32 @@ extends Node
 @export var options_button: Button
 @export var options_menu: RightSideMenu
 
+@export var animation_player: AnimationPlayer
+@export var tree_area: Area2D
+@export var tree_sprite: Sprite2D
+@export var tree_marker: Marker2D
+@export var character_marker: Marker2D
+@export var steal_sfx: AudioStream
+
+var _is_hovering_tree: bool = false
+
 
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_button_pressed)
 	options_button.pressed.connect(_on_options_button_pressed)
 	
+	tree_area.mouse_entered.connect(_on_tree_area_mouse_entered)
+	tree_area.mouse_exited.connect(_on_tree_area_mouse_exited)
+	
+	animation_player.play(&"character")
 	play_button.grab_focus()
+
+
+func _process(delta: float) -> void:
+	if _is_hovering_tree:
+		if Input.is_action_pressed("steal"):
+			ParticleManager.generate(tree_marker.global_position, character_marker, steal_sfx)
+			animation_player.advance(-3.0 * delta)
 
 
 func toggle_level_menu() -> void:
@@ -46,3 +66,13 @@ func _on_play_button_pressed() -> void:
 func _on_options_button_pressed() -> void:
 	options_button.grab_focus()
 	toggle_options_menu()
+
+
+func _on_tree_area_mouse_entered() -> void:
+	_is_hovering_tree = true
+	tree_sprite.set_instance_shader_parameter("is_enabled", true)
+
+
+func _on_tree_area_mouse_exited() -> void:
+	_is_hovering_tree = false
+	tree_sprite.set_instance_shader_parameter("is_enabled", false)
