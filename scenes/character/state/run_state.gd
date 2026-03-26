cc3f138b8223cd7485da89bd4ae2c5ba8748d6c1
LivingDood <living.dood.dev@gmail.com>
2026-03-26 16:02:31 +0100

    Add fall checks during idle and run (#2)
    Co-authored-by: LivingDood <264967182+LivingDood@users.noreply.github.com>

--- scenes/character/state/run_state.gd
+++ scenes/character/state/run_state.gd
@@ -10,6 +10,10 @@ func enter() -> void:
 func update(_delta: float) -> void:
 	_player.sprite.flip_h = not _player.direction_component.is_looking_right
 	
+	if _player.jump_component.is_falling:
+		_player.state_machine.change_state("fall")
+		return
+	
 	if not _player.move_component.is_moving:
 		_player.state_machine.change_state("idle")
 		return
