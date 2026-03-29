d709e64318333164b3256b90befbb5c61daedf9e
LivingDood <living.dood.dev@gmail.com>
2026-03-29 19:58:38 +0200

    Update mushroom visuals
    Co-authored-by: LivingDood <264967182+LivingDood@users.noreply.github.com>

--- scenes/objects/stealable.gd
+++ scenes/objects/stealable.gd
@@ -1,6 +1,17 @@
-@abstract class_name Stealable
+@abstract
+class_name Stealable
 extends AnimatableBody2D
-var time_transfer_multiplier:float = 1.
-@abstract func steal(seconds:float) -> float
-@abstract func give(seconds:float) -> float
-@abstract func set_highlight(enabled:bool) -> void
+
+var time_transfer_multiplier: float = 1.0
+
+
+@abstract
+func steal(seconds: float) -> float
+
+
+@abstract
+func give(seconds: float) -> float
+
+
+@abstract
+func set_highlight(enabled: bool) -> void
